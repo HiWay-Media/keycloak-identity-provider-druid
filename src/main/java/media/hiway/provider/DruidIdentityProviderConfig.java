1ac113c90301b7bc47376ee589a5c94595ab0fdc
Allan Nava <allannava95@gmail.com>
2024-12-18 12:01:21 +0100

    updated DruidIdentityProviderConfig

--- src/main/java/media/hiway/provider/DruidIdentityProviderConfig.java
+++ src/main/java/media/hiway/provider/DruidIdentityProviderConfig.java
@@ -9,6 +9,19 @@ class DruidIdentityProviderConfig extends OIDCIdentityProviderConfig {
     DruidIdentityProviderConfig(IdentityProviderModel identityProviderModel) {
         super(identityProviderModel);
     }
+    private static final String PROD_ENV = "prodEnv";
+    private static final String DISPLAY_ICON_CLASSES = "fa fa-dragon";
+    private static final String DISPLAY_NAME = "displayName";
+    private static final String DEFAULT_DISPLAY_NAME = "Sign in with Druid";
+    // need to setup if is debug or prod urls
+     public String getProd() {
+        return getConfig().get(PROD_ENV);
+    }
+
+    public void setProd(String isProd) {
+        getConfig().put(PROD_ENV, isProd);
+    }
+
 
     public String getKeyId() {
         return getConfig().get("keyId");
@@ -16,5 +29,24 @@ class DruidIdentityProviderConfig extends OIDCIdentityProviderConfig {
 
     public String getTeamId() {
         return getConfig().get("teamId");
+    }
+
+    @Override
+    public void setDisplayName(String displayName) {
+        getConfig().put(DISPLAY_NAME, displayName);
+    }
+
+    @Override
+    public String getDisplayName() {
+        var displayName = getConfig().get(DISPLAY_NAME);
+        if (displayName == null || displayName.isBlank()) {
+            return DEFAULT_DISPLAY_NAME;
+        }
+        return displayName;
+    }
+
+    @Override
+    public String getDisplayIconClasses() {
+        return DISPLAY_ICON_CLASSES;
     }
 }
