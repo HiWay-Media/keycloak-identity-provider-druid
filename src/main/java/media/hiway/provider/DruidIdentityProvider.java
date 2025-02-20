e9ee7ff9805473deb4c0e53a488672bb684014fe
Allan Nava <allannava95@gmail.com>
2025-02-20 12:58:41 +0100

    added more logs

--- src/main/java/media/hiway/provider/DruidIdentityProvider.java
+++ src/main/java/media/hiway/provider/DruidIdentityProvider.java
@@ -125,13 +125,13 @@ public class DruidIdentityProvider extends AbstractOAuth2IdentityProvider<DruidI
         UriBuilder uriBuilder = super.createAuthorizationUrl(request);
         final DruidIdentityProviderConfig config = (DruidIdentityProviderConfig) getConfig();
         logger.infof("createAuthorizationUrl config: %s", config);
-        
-        logger.infof("request %v %s", request.getHttpRequest(), request.getHttpRequest());
+        logger.infof("createAuthorizationUrl request %v %s", request.getHttpRequest(), request.getHttpRequest());
         // debug logger
         String xMethod = request.getHttpRequest().getDecodedFormParameters().getFirst("x_method");
         String scope = request.getHttpRequest().getDecodedFormParameters().getFirst("scope");
         logger.infof("request x_method: %s", xMethod);
         logger.infof("request scope %s", scope);
+        logger.infof("decoded ", request.getHttpRequest().getDecodedFormParameters());
 
         uriBuilder.queryParam(OAUTH2_PARAMETER_STATE, request.getState().getEncoded())
             .queryParam(OAUTH2_PARAMETER_RESPONSE_TYPE, "code")
